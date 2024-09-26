42712e90a920db45388e4790dc2fbbbd58bb0ad2
augustine76 <gh@abi.run>
2024-09-26 14:46:59 -0400

    added: isPrime()

--- main.go
+++ main.go
@@ -19,3 +19,18 @@ func BubbleSort(arr []int) {
 		}
 	}
 }
+
+//Abhijith Augustine
+//IsPrime function checks if a number is prime or not
+
+func isPrime(n int) bool {
+	if n <= 1 {
+		return false
+	}
+	for i := 2; i*i <= n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
