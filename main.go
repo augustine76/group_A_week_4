39d09c834b3d76dd8972299d67388261a43f6824
Bishal <yogibishal19@gmail.com>
2024-09-26 15:20:03 -0400

    Added factorial

--- main.go
+++ main.go
@@ -63,3 +63,19 @@ func fibonacci(n int) int {
 	}
 	return fibonacci(n-1) + fibonacci(n-2)
 }
+
+// Bishal ZOgi
+// factorial calculates the factorial of a non-negative integer n.
+func factorial(n int) int {
+	if n < 0 {
+		return -1 // return -1 for negative inputs to indicate an error
+	}
+	if n == 0 {
+		return 1 // factorial of 0 is 1
+	}
+	result := 1
+	for i := 1; i <= n; i++ {
+		result *= i
+	}
+	return result
+}
