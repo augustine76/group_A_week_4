45567f97c7b7957d848d814e37ece220e6329f23
Rohit Sthapit <nhuzaa@users.noreply.github.com>
2024-09-27 00:25:08 +0545

    Added Bubble Sort

--- main.go
+++ main.go
@@ -5,3 +5,17 @@ import "fmt"
 func main() {
 	fmt.Println("Welcome to Group I's Week 4 Project!")
 }
+
+// Rohit Sthapit
+// BubbleSort function sorts a slice of integers using the Bubble Sort algorithm
+func BubbleSort(arr []int) {
+	n := len(arr)
+	for i := 0; i < n-1; i++ {
+		for j := 0; j < n-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				// Swap arr[j] and arr[j+1]
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+}
